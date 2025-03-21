33a7f730618991de2900caf89309d4072e5a29ca
dozo <dozothebozo@gmail.com>
2025-03-21 02:17:14 -0400

    feat: add zustand & test usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useCountStore } from "./store"
+
 function App() {
+  const count = useCountStore((state) => state.count)
+  const increment = useCountStore((state) => state.increment)
+  const decrement = useCountStore((state) => state.decrement)
+
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
+        {count}
       </h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
     </>
   )
 }
