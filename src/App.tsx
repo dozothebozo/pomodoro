cfaa2175edc09220b14aeb932f6068d3b16a623f
dozo <dozothebozo@gmail.com>
2025-03-23 02:13:41 -0400

    refactor: change from css to tailwind to apply full viewport

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ function App() {
 
   return (
     <>
-      <div className="content">
-        <h1 className="text-3xl font-bold underline text-red-500">
-          {count}
-        </h1>
-        <button onClick={increment}>increment</button>
-        <button onClick={decrement}>decrement</button>
+      <div className="h-screen min-h-screen overflow-y-scroll bg-yellow-300">
+        <div className="content">
+          <h1 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl font-bold underline text-red-500">
+            {count}
+          </h1>
+          <button onClick={increment}>increment</button>
+          <button onClick={decrement}>decrement</button>
+        </div>
       </div>
     </>
   )
